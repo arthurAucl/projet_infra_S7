99228c565e8c71d20f3c359229cee968930d315a
arthurAucl <81313514+arthurAucl@users.noreply.github.com>
2023-01-17 08:35:47 +0100

    affichage nouvelle db

--- data/www/index.php
+++ data/www/index.php
@@ -19,7 +19,7 @@ if (!$conn) {
    die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT * FROM Salle";
+$sql = "SELECT * FROM salle";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
@@ -27,47 +27,47 @@ if (mysqli_num_rows($result) > 0) {
    echo "Table : Salle <br>";
    while($row = mysqli_fetch_assoc($result)) {
       //echo "id: " . $row["id"]. " - Entry: " . $row["entry"]. "<br>";
-      echo "id: " . $row["id"]. " - Nom de la salle: " . $row["nomDeLaSalle"]. " - Batiment: " . $row["batiment"]. " - Numero: " . $row["numero"]. " - Disponible: " . $row["disponible"]."<br>";
+      echo "id: " . $row["id"]. " - Nom de la salle: " . $row["nom"]. " - Batiment: " . $row["batiment"]. " - Numero: " . $row["numero"]. " - Disponible: " . $row["disponible"]."<br>";
       
    }
 } else {
    echo "0 results";
 }
 
-$sql = "SELECT * FROM Utilisateurs";
+$sql = "SELECT * FROM utilisateurs";
 $result = mysqli_query($conn, $sql); 
  
 if (mysqli_num_rows($result) > 0) {
    // output data of each row
    echo "<br> Table : Utilisateurs <br>";
    while($row = mysqli_fetch_assoc($result)) {
-      echo "id: " . $row["id"]. " - ID de l'utilisateur: " . $row["id_user"]. " - Nom: " . $row["nom"]. " - Prenom: " . $row["prenom"]. " - Email: " . $row["email"].  " - Mot de passe: " . $row["motDePasse"].  " - Situation: " . $row["etat"]."<br>";
+      echo "id: " . $row["id"]. " - Nom: " . $row["nom"]. " - Prenom: " . $row["prenom"]. " - Email: " . $row["email"].  " - Mot de passe: " . $row["motDePasse"].  " - Situation: " . $row["etat"]."<br>";
    }
 } else {
    echo "0 results";
 }
 
-$sql = "SELECT * FROM Materiels";
+$sql = "SELECT * FROM materiels";
 $result = mysqli_query($conn, $sql); 
  
 if (mysqli_num_rows($result) > 0) {
    // output data of each row
    echo "<br> Table : Materiels <br>";
    while($row = mysqli_fetch_assoc($result)) {
-      echo "id: " . $row["id"]. " - nomDuMateriel: " . $row["nomDuMateriel"]. " - description_materiel: " . $row["description_materiel"]. " - qualiteAvantLePret: " . $row["qualiteAvantLePret"]. " - qualiteApresLePret: " . $row["qualiteApresLePret"].  " - prix: " . $row["prix"].  " - dateAttribution: " . $row["dateAttribution"]. " - dateRestitution: " . $row["dateRestitution"]."<br>";
+      echo "id: " . $row["id"]. " - nomDuMateriel: " . $row["nomDuMateriel"]. " - photo: " . $row["photo"]. " - prix: " . $row["prix"]. " - qualite: " . $row["qualite"].  " - disponible: " . $row["disponible"]."<br>";
    }
 } else {
    echo "0 results";
 }
 
-$sql = "SELECT * FROM RDV";
+$sql = "SELECT * FROM rdv";
 $result = mysqli_query($conn, $sql); 
  
 if (mysqli_num_rows($result) > 0) {
    // output data of each row
    echo "<br> Table : Rendez-vous <br>";
    while($row = mysqli_fetch_assoc($result)) {
-      echo "id: " . $row["id"]. " - id_rdv: " . $row["id_rdv"]. " - description_rdv: " . $row["description_rdv"]. " - salle_id: " . $row["salle_id"]. " - utilisateurDemande_id: " . $row["utilisateurDemande_id"].  " - utilisateurSollicite_id: " . $row["utilisateurSollicite_id"].  " - dateEtHeure: " . $row["dateEtHeure"]. " - duree: " . $row["duree"]."<br>";
+      echo "id: " . $row["id"]. " - date: " . $row["date"]. " - durée: " . $row["duree"]. " - description: " . $row["description"]. " - etat: " . $row["etat"].  " - salle: " . $row["salle"]."<br>";
    }
 } else {
    echo "0 results";
