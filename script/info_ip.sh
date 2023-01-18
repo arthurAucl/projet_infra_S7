c24184e86053d3397633d38672e53d196bead65e
arthurAucl <81313514+arthurAucl@users.noreply.github.com>
2023-01-18 08:43:48 +0100

    Update info_ip.sh

--- script/info_ip.sh
+++ script/info_ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "IP 192.168.56.80 pour le Proxy"
-echo "IP de 192.168.56.81 a 192.168.56.83 pour le web"
+echo "IP de 192.168.56.81 à 192.168.56.83 pour le web"
 echo "IP 192.168.56.84 pour database"
 echo "IP 192.168.56.85 pour le backup"
